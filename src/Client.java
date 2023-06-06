e6e5ef5317387b857af837a40045089b64a6f9fb
Iago Roque <71741741+iagoroque@users.noreply.github.com>
2023-06-06 15:17:41 -0300

    Pendências 1 e 2 resolvidas
    Agora todos os métodos de ordenação estão ordenando qualquer tipo de array, e o método merge que estávamos usando foi substituído por outro tipo de método merge também, pois estava apresentando falha, e agora não está mais.

--- src/Client.java
+++ src/Client.java
@@ -6,59 +6,51 @@ import sortCommand.*;
 public class Client {
     public static void main(String[] args) {
 
-        /*
-         * Pendências:
-         * 
-         * 1 - Os métodos de ordenaçao só estão funcionando em arrays inversamente
-         * ordenados
-         * 
-         * 2 - O método mergeSort não funciona nem mesmo com o vetor iversamente
-         * ordenado.
-         * 
-         * 3 - Organizar melhor a exibição das informações dos tempos de ordenação. / a
-         * contagemde tempo não está
-         * funcionando com o template Method
-         * 
-         * ------------------------------------------------------
-         * 
-         * 
-         */
-
-        long tempoInicial = 0;
-        long tempoFinal = 0;
-
-        ArrayFactory inverseArrayFactory = new InverseArrayFactory();
+        ArrayFactory sortedArrayFactory = new SortedArrayFactory();
         ArrayFactory randomArrayFactory = new RandomArrayFactory();
+        ArrayFactory inverseArrayFactory = new InverseArrayFactory();
+        ArrayFactory almostSortedArrayFactory = new AlmostSortedArrayFactory();
 
-        Array inverseArray10 = inverseArrayFactory.createArray(10);
-        Array inverseArray100 = inverseArrayFactory.createArray(100);
-        Array inverseArray1k = inverseArrayFactory.createArray(1000);
-        Array inverseArray10k = inverseArrayFactory.createArray(10000);
-
+        Array sortedArray10 = sortedArrayFactory.createArray(10);
         Array randomArray10 = randomArrayFactory.createArray(10);
+        Array inverseArray10 = inverseArrayFactory.createArray(10);
+        Array almostSortedArray10 = almostSortedArrayFactory.createArray(10);
 
         SortInvoker invoker = new SortInvoker();
 
         Metrics metrics = new MetricTemplate();
 
-        // tempoInicial = System.currentTimeMillis();
-
-        invoker.setCommand(new MergeSortCommand(inverseArray100.returnArray(), 0, 100));
+        invoker.setCommand(new MergeSortCommand(sortedArray10.returnArray(), sortedArray10.returnArray().length));
         Integer[] array = invoker.executeCommand(metrics);
-        // Integer[] array = inverseArray10.returnArray();
-
-        // metric.ShowMetrics();
-
-        // tempoFinal = System.currentTimeMillis();
-
-        // System.out.println("\nO tempo necessário para a ordenação deste array é de: "
-        // +
-        // (tempoFinal - tempoInicial) + " milisegundos.");
-
         for (int i = 0; i < array.length; i++) {
-        System.out.println(array[i]);
+            System.out.print(array[i] + " ");
         }
 
+        invoker.setCommand(new MergeSortCommand(randomArray10.returnArray(), randomArray10.returnArray().length));
+        array = invoker.executeCommand(metrics);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        invoker.setCommand(new MergeSortCommand(inverseArray10.returnArray(), inverseArray10.returnArray().length));
+        array = invoker.executeCommand(metrics);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        invoker.setCommand(
+                new MergeSortCommand(almostSortedArray10.returnArray(), almostSortedArray10.returnArray().length));
+        array = invoker.executeCommand(metrics);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
     }
 
+    /*
+     * Pendências:
+     * 
+     * 3 - Organizar melhor a exibição das informações dos tempos de ordenação. / a
+     * contagemde tempo não está
+     * funcionando com o template Method
+     */
 }
--- src/sortCommand/MergeSortCommand.java
+++ src/sortCommand/MergeSortCommand.java
@@ -3,63 +3,74 @@ package sortCommand;
 public class MergeSortCommand implements Command {
 
     public Integer[] array;
-    Integer inicio;
-    Integer fim;
+    Integer tamanho;
 
-    public MergeSortCommand(Integer[] array, Integer inicio, Integer fim) {
+    public MergeSortCommand(Integer[] array, Integer tamanho) {
         this.array = array;
-        this.inicio = inicio;
-        this.fim = fim;
+        this.tamanho = tamanho;
     }
 
     public Integer[] execute() {
-        mergeSort(this.array, inicio, fim);
+        Integer[] array = this.array;
+        mergeSort(array, tamanho);
         return array;
     }
 
-    public void mergeSort(Integer[] x, Integer inicio, Integer fim) {
-        Integer meio;
-        if (inicio < fim) {
-            meio = (inicio + fim) / 2;
-            mergeSort(x, inicio, meio);
-            mergeSort(x, meio + 1, fim);
-            intercala(x, inicio, meio, fim);
+    private static void mergeSort(Integer[] vetor, Integer tamanho) {
+        Integer elementos = 1;
+        Integer inicio, meio, fim;
+
+        while (elementos < tamanho) {
+            inicio = 0;
+
+            while (inicio + elementos < tamanho) {
+                meio = inicio + elementos;
+                fim = inicio + 2 * elementos;
+
+                if (fim > tamanho)
+                    fim = tamanho;
+
+                intercala(vetor, inicio, meio, fim);
+
+                inicio = fim;
+            }
+
+            elementos = elementos * 2;
         }
     }
 
-    private void intercala(Integer[] vetor, Integer inicio, Integer meio, Integer fim) {
-        Integer array[] = new Integer[fim - inicio];
+    private static void intercala(Integer[] vetor, Integer inicio, Integer meio, Integer fim) {
+        Integer novoVetor[] = new Integer[fim - inicio];
         Integer i = inicio;
         Integer m = meio;
         Integer pos = 0;
 
         while (i < meio && m < fim) {
             if (vetor[i] <= vetor[m]) {
-                array[pos] = vetor[i];
+                novoVetor[pos] = vetor[i];
                 pos = pos + 1;
                 i = i + 1;
             } else {
-                array[pos] = vetor[m];
+                novoVetor[pos] = vetor[m];
                 pos = pos + 1;
                 m = m + 1;
             }
         }
 
         while (i < meio) {
-            array[pos] = vetor[i];
+            novoVetor[pos] = vetor[i];
             pos = pos + 1;
             i = i + 1;
         }
 
         while (m < fim) {
-            array[pos] = vetor[m];
+            novoVetor[pos] = vetor[m];
             pos = pos + 1;
             m = m + 1;
         }
 
         for (pos = 0, i = inicio; i < fim; i++, pos++) {
-            vetor[i] = array[pos];
+            vetor[i] = novoVetor[pos];
         }
     }
-
 }
